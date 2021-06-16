ca42f2fcad2f78366260f67d6ce91c2dce72f05e
Joshuakakembo <79513004+Joshuakakembo@users.noreply.github.com>
2021-06-16 13:26:17 +0300

    MyCommit

---
+++ ASPNETCoreReactNET-Example/wwwroot/Source/Components/ExampleComponent.jsx
@@ -0,0 +1,11 @@
+﻿// This is our component.
+class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div className="testDiv">
+                <h1>{this.props.user.name}</h1>
+                <h3>{this.props.user.age}</h3>   
+            </div>
+        );
+    }
+}
